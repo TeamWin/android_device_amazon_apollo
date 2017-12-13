83191de5276ce8cd5d2275f4a353e892bb785d61
More Robust Than You <MoreRobustThanYou@users.noreply.github.com>
2017-12-12 20:03:05 -0500

    twrpconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,11 +5,7 @@
 -include vendor/amazon/hdx-common/BoardConfigVendor.mk
 
 # Kernel
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-TARGET_KERNEL_CONFIG := apollo_eng_defconfig
-else
-TARGET_KERNEL_CONFIG := apollo_defconfig
-endif
+TARGET_KERNEL_CONFIG := apollo_twrp_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 mdss_mdp.panel=0:qcom,mdss_dsi_jdi_dualmipi0_video:1:qcom,mdss_dsi_jdi_dualmipi1_video:
 
 # Assert
